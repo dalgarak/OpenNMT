2222ae84f4c5b75c8785fbbec29b161dc08c216f
JH Shin <luna.jetch@gmail.com>
2017-03-30 14:23:30 +0900

    xmlrpc server update : add BPE tokenizer routine.. but, we need to rewrite server

--- tools/xmlrpc-translation-server.lua
+++ tools/xmlrpc-translation-server.lua
@@ -31,6 +31,9 @@ local cmd = onmt.utils.ExtendedCmdLine.new('xmlrpc_translation_server.lua')
 -- translator를 밖으로 빼놓음
 local translator
 
+-- settings
+local is_mixed = false
+
 onmt.translate.Translator.declareOpts(cmd)
 
 onmt.utils.Cuda.declareOpts(cmd)
@@ -43,7 +46,10 @@ cmd:text("")
 cmd:option('-config', '', [[Read options from this file]])
 cmd:option('-host', '*', [[Host to run the server on]])
 cmd:option('-port', '8020', [[Port to run the server on]])
-cmd:option('-extra_digit', 'false', [[If it enabled, comma between numerics will be omitted.]])
+cmd:option('-extra_digit', false, [[If it enabled, comma between numerics will be omitted.]])
+cmd:option('-mixed', false, [[If it enabled, all OOVs will be splitted into consequence of its characters.]])
+cmd:option('-bpe_model', '', [[Byte-pair encoding scheme based tokenization]])
+cmd:option('-use_opennmt_joiner', false, [[If it enabled, use opennmt's joiner instead B/M/E symbols.]])
 
 -- XML-RPC WSAPI Handler
 function wsapi_handler(wsapi_env)
@@ -54,12 +60,13 @@ function wsapi_handler(wsapi_env)
       들어오기 때문에, [1]로 한번 unpacking을 해 줘야 한다.
   ]]
   local func = xmlrpc.dispatch(method)
-  local result = { pcall(func, arg_table[1]["text"]) }
+  local result = { pcall(func, arg_table[1]["text"], is_mixed) }
   --print ('result table: ', result)
   local is_ok = result[1]	-- pcall의 처리 결과가 나온다
   local final_result = {}
 
   if not is_ok then
+    print('err: ' .. result[2])
     result = { code = 3, message = result[2] }
   else
     final_result["text"] = "<![CDATA[" .. result[2]:gsub("&", "%%26") .. "]]>";
@@ -83,10 +90,20 @@ xmlrpc_exports = {}
 function digit_placeholder_prep(input_text)
   local num_idx = 1
   local num_tab = {}
+  local sidx = 0
+  local eidx = 0
+
+  -- 전화번호를 011 - 4573 - 1234 와 같이 떨어트리는 경우를 위해, 연속한 것을 붙이도록 한다.
+  -- input_text = lrexonig.gsub(input_text, "(?<=[0-9]) - (?=[0-9])", "-")
+
   while true do
 	-- 한국어 월 앞의 숫자는 매칭되지 않게 한다.
 	-- sidx, eidx = lrexonig.find(input_text, "(?<!_)(?>[0-9]+)")
-	local sidx, eidx = lrexonig.find(input_text, "(?<!_)(?>[0-9]+)(?! 월)")
+	if eidx ~= 0 then
+          eidx = sidx + 10
+        end
+	--sidx, eidx = lrexonig.find(input_text, "(?<![_,.])[0-9][0-9.,-]*(?! 월)")  -- 아직 쓰지 말것. . , merging이 똑바로 안되어 있다.
+	sidx, eidx = lrexonig.find(input_text, "(?<!_)(?>[0-9]+)(?! 월)")    -- old style (e.g. ke-25m-digit)
 	if sidx == nil then 
 		break 
 	end
@@ -97,36 +114,85 @@ function digit_placeholder_prep(input_text)
 		input_text = input_text:sub(1, sidx-1) .. "__JUST!ONE!_" .. input_text:sub(eidx+1, #input_text)
 		goto continue 
 	end
+	print (num_idx .. ':' .. a_found_value)
 
 	num_tab[num_idx] = a_found_value
 
 	if id_digit_align_enable == true then
+		--input_text = input_text:sub(1, sidx-1) .. "__digit_" .. tostring(num_idx) .. "__ " .. input_text:sub(eidx+1, #input_text) 
 		input_text = input_text:sub(1, sidx-1) .. "__digit_" .. tostring(num_idx) .. " " .. input_text:sub(eidx+1, #input_text) 
-		-- input_text = lrexonig.gsub(input_text, "(?<!_)(?[0-9]+", "__digit_" .. tostring(num_idx) .. " ", 1)
 	else
 		input_text = lrexonig.gsub(input_text, "(?<!_)[0-9]+", "__digit_", 1)
 	end
+	--print('intermediate - ' .. input_text)
 	num_idx = num_idx + 1
+	if num_idx > 9 then
+		break
+	end
 	::continue::
   end
 
   -- recover single number "1"
   input_text = input_text:gsub("__JUST!ONE!_", "1")
   input_text = input_text:gsub("  ", " ")
+  
+  print('modified input: ' .. input_text)
 
   return input_text, num_tab
+end
+
+function break_oov_into_chars(input_word)
+  local unicode = require('tools.utils.unicode')
+  local new_a_word = ''
+  --print (input_word)
+  local t = 1
+  for _, c, _, nextc in unicode.utf8_iter(input_word) do
+    if is_use_opennmt_joiner then
+      if t ~= 1 then
+        new_a_word = new_a_word .. ' ' .. require('tools.utils.separators').joiner_marker .. c
+      else
+        new_a_word = c
+      end
+    else
+      --print(c)
+      if t == 1 then
+        new_a_word = '<B>' .. c
+      elseif nextc == nil then
+        new_a_word = new_a_word .. ' <E>' .. c
+      else
+        new_a_word = new_a_word .. ' <M>' .. c
+      end
+    end
+    t = t + 1
+  end
+  return new_a_word
 end
 
 -- translate method
 --[[ 
       Each XML-RPC procedures returns pcall result(true/false), return of procedures 1, return of procedures 2 ... as a lua table.
   ]]
-function xmlrpc_exports.translate(input_text)
+function xmlrpc_exports.translate(input_text, is_mixed_param)
+  print('Start Translate..')
   local unk_penalty = 2 
   local batch = {}
 
   -- Input text의 숫자를 읽어들여, 이를 placeholder로 바꿔주도록 한다.
-  local pp_input_text, num_tab = digit_placeholder_prep(input_text)
+  local pp_input_text, num_tab
+  if _G.bpe ~= nil and _G.tokenizer ~= nil then
+    local separators = require('tools.utils.separators')
+    local bpeopt = {
+      joiner_annotate = true,
+      joiner = separators.joiner_marker,
+      joiner_new = false
+    }
+    local tmpTokens = _G.tokenizer.tokenize(bpeopt, input_text, _G.bpe)
+    pp_input_text = table.concat(tmpTokens, ' ')
+    print('BPE-ed input text: ' .. pp_input_text)
+  else
+    pp_input_text, num_tab = digit_placeholder_prep(input_text)
+  end
+  
 
   -- tokenize & feature extraction
   local srcTokens = {}
@@ -136,15 +202,58 @@ function xmlrpc_exports.translate(input_text)
 
   table.insert(batch, translator:buildInput(srcTokens))
 
-  -- data.src에 idx화 된 데이터가 들어 있다
+  -- 170302. translator:buildData Added, to access
   pla = require 'pl.pretty'
   local unk_word_cnt = 0
-  dsrc = data.words[1]:storage()
-  --print (pla.dump(dsrc))
-  for i = 1, dsrc:size() do
-    if dsrc[i] == onmt.Constants.UNK then
-      unk_word_cnt = unk_word_cnt + 1
+  local data = translator:buildData(batch) 
+
+  if is_mixed_param then
+    pp_input_text = ''
+  end
+  --print (pla.dump(data))
+  local idxed_words = data.src[1]
+  for i = 1, idxed_words:storage():size() do
+    if idxed_words[i] == onmt.Constants.UNK then
+      print('unk found.' .. srcTokens[i])
+      if is_mixed_param then
+	print(srcTokens[i])
+        local new_word = break_oov_into_chars(srcTokens[i])
+	if i > 1 then
+          pp_input_text = pp_input_text .. ' '
+        end  
+	pp_input_text = pp_input_text .. new_word
+      else
+        unk_word_cnt = unk_word_cnt + 1
+      end
+    else
+      if is_mixed_param then
+	if i > 1 then
+          pp_input_text = pp_input_text .. ' '
+        end  
+        pp_input_text = pp_input_text .. srcTokens[i]
+      end
     end 
+  end
+  print('unk word count: ' .. unk_word_cnt)
+
+  if is_mixed_param and unk_word_cnt >= 1 then
+    print('mixed - rebuild input start.')
+    srcTokens = {}
+    batch = {}
+    for word in pp_input_text:gmatch'([^%s]+)' do
+      table.insert(srcTokens, word)
+    end
+    print ('-mixed: ' .. pp_input_text)
+
+    table.insert(batch, translator:buildInput(srcTokens))
+    local data = translator:buildData(batch) 
+    unk_word_cnt = 0
+    local idxed_words = data.src[1]
+    for i = 1, idxed_words:storage():size() do
+      if idxed_words[i] == onmt.Constants.UNK then
+        unk_word_cnt = unk_word_cnt + 1
+      end
+    end
   end
 
   -- Translate
@@ -178,8 +287,10 @@ function xmlrpc_exports.translate(input_text)
   while true do
 	if id_digit_align_enable == true then
 		-- 원래 __digit_[0-9]+ 가 되어야 하나, 영어쪽 corpus 수정 실패로 인해 임시로 수정. 10개는 넘어가지 않을 것이다.
+		--sidx, eidx = pred_result:find("__digit_[0-9]__") 	-- enable when ko-en testing 365m
 		sidx, eidx = pred_result:find("__digit_[0-9]")
 		if sidx == nil then break end
+		--an_id = pred_result:sub(sidx+8, eidx-2)		-- enable with ko-en testing 365m
 		an_id = pred_result:sub(sidx+8, eidx)
 		-- 숫자 다음에 0이 오면 공백을 제거
 		print (#pred_result)
@@ -192,7 +303,7 @@ function xmlrpc_exports.translate(input_text)
 		    end
 		end
 		if num_tab[tonumber(an_id)] == nil then
-			pred_result = ""
+			--pred_result = ""
 			pred_score = -150.0
 			normed_score = 0.00000001
 			break
@@ -205,6 +316,18 @@ function xmlrpc_exports.translate(input_text)
 		pred_result = pred_result:gsub("__digit_", num_tab[recov_num_idx], 1)
 	end
 	recov_num_idx = recov_num_idx + 1
+  end
+
+  if is_mixed then
+    pred_result = pred_result:gsub('<B>', '')
+    pred_result = pred_result:gsub(' <M>', '')
+    pred_result = pred_result:gsub(' <E>', '')
+    pred_result = pred_result:gsub(' ￭', '')
+  end
+
+  if _G.bpe ~= nil then
+    pred_result = pred_result:gsub('￭ ', '')
+    pred_result = pred_result:gsub(' ￭', '')
   end
 
   -- unk penalty를 가함 --
@@ -229,9 +352,23 @@ local function main()
 
   local opt = cmd:parse(arg)
 
+  is_mixed = opt.mixed
+
   _G.logger = onmt.utils.Logger.new(opt.log_file, opt.disable_logs, opt.log_level)
 
   _G.logger:info("Initializing NMT Model, Please wait for a while...")
+
+  if is_mixed then
+    _G.logger:info('Mixed Word/Character Mode enabled.')
+  end
+
+  -- BPE를 적용한다.
+  if #opt.bpe_model > 0 then
+    _G.BPE = require ('tools.utils.BPE')
+    _G.tokenizer = require('tools.utils.tokenizer')
+    _G.bpe = _G.BPE.new(opt.bpe_model, true, false)
+    _G.logger:info('BPE model enabled: joiner_annotate - true mode')
+  end
 
   -- XML-RPC Handler에서 접근하기 위해, translator는 global로 잡는다
   translator = onmt.translate.Translator.new(opt)
